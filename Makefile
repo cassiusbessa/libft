060ae90562ccfe3e0effb05ae612c2ba3e185fc7
caqueiro <cassiusbessa@gmail.com>
2023-10-25 20:43:01 -0300

    strtrim

--- Makefile
+++ Makefile
@@ -24,7 +24,8 @@ SRCS	= ft_isalpha.c\
 	  ft_calloc.c\
 	  ft_strdup.c\
 	  ft_substr.c\
-	  ft_strjoin.c
+	  ft_strjoin.c\
+	  ft_strtrim.c
 	  
 OBJS	= ${SRCS:.c=.o}
 
--- ft_strtrim.c
+++ ft_strtrim.c
@@ -10,16 +10,35 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-/*#include "libft.h"
+#include "libft.h"
+
+char	*ft_strchr(const char *s, int c);
+char	*ft_substr(char const *s, unsigned int start, size_t len);
 
 char	*ft_strtrim(char const *s1, char const *set)
 {
 	size_t	i;
-	size_t	c;
-	char	*t;
-	i = 0;
-	while (s1[i++])
-	{
-		if (s1[i] == 	
-*/	
+	size_t	e;
 
+	i = 0;
+	e = 0;
+	if (s1 == NULL || set == NULL)
+		return (0);
+	while (s1[i] && ft_strchr(set, s1[i]))
+		i++;
+	e = i;
+	while (s1[e])
+		e++;
+	if (e == i)
+		return (ft_strdup(""));
+	while (ft_strchr(set, s1[e]))
+		e--;
+	return (ft_substr(s1, i, (e - i) + 1));
+}
+/*int main(void)
+{
+	char *s1 = "";
+	char *s2 = "";
+	char *ret = ft_strtrim(s1, s2);
+    printf("%s", ret);
+}*/
